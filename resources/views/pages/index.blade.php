744600e0c3be3a32fce860dd1d2ce911e219cd39
arianto-510 <97211033@student.itk.ac.id>
2023-10-03 17:48:18 +0800

    #0001 : Template - Slicing Template

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,191 @@
+@extends('layouts.index')
+@section('content')
+    <!-- Carousel Start -->
+    <div class="container-fluid px-0">
+        <div id="carouselId" class="carousel slide" data-bs-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-bs-target="#carouselId" data-bs-slide-to="0" class="active" aria-current="true"
+                    aria-label="First slide"></li>
+                <li data-bs-target="#carouselId" data-bs-slide-to="1" aria-label="Second slide"></li>
+            </ol>
+            <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                    <img src="{{ asset('front-end/img/carousel-1.jpg') }}" class="img-fluid" alt="First slide">
+                    <div class="carousel-caption">
+                        <div class="container carousel-content">
+                            <h6 class="text-secondary h4 animated fadeInUp">Best IT Solutions</h6>
+                            <h1 class="text-white display-1 mb-4 animated fadeInRight">An Innovative IT Solutions
+                                Agency</h1>
+                            <p class="mb-4 text-white fs-5 animated fadeInDown">Lorem ipsum dolor sit amet elit. Sed
+                                efficitur quis purus ut interdum. Pellentesque aliquam dolor eget urna ultricies
+                                tincidunt.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="{{ asset('front-end/img/carousel-2.jpg') }}" class="img-fluid" alt="Second slide">
+                    <div class="carousel-caption">
+                        <div class="container carousel-content">
+                            <h6 class="text-secondary h4 animated fadeInUp">Best IT Solutions</h6>
+                            <h1 class="text-white display-1 mb-4 animated fadeInLeft">Quality Digital Services You
+                                Really Need!</h1>
+                            <p class="mb-4 text-white fs-5 animated fadeInDown">Lorem ipsum dolor sit amet elit. Sed
+                                efficitur quis purus ut interdum. Pellentesque aliquam dolor eget urna ultricies
+                                tincidunt.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+
+    <!-- About Start -->
+    <div class="container-fluid py-5 my-5">
+        <div class="container pt-5">
+            <div class="row g-5">
+                <div class="col-lg-5 col-md-6 col-sm-12 wow fadeIn" data-wow-delay=".3s">
+                    <div class="h-100 position-relative">
+                        <img src="{{ asset('front-end/img/about-1.jpg') }}" class="img-fluid w-75 rounded" alt=""
+                            style="margin-bottom: 25%;">
+                        <div class="position-absolute w-75" style="top: 25%; left: 25%;">
+                            <img src="img/about-2.jpg" class="img-fluid w-100 rounded" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-7 col-md-6 col-sm-12 wow fadeIn" data-wow-delay=".5s">
+                    <h5 class="text-primary">Tentang BEM FTI</h5>
+                    <h1 class="mb-4">Badan Eksekutif Mahasiswa Fakultas Teknologi Informasi</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur quis purus ut interdum.
+                        Pellentesque aliquam dolor eget urna ultricies tincidunt. Nam volutpat libero sit amet leo
+                        cursus, ac viverra eros tristique. Morbi quis quam mi. Cras vel gravida eros. Proin scelerisque
+                        quam nec elementum viverra. Suspendisse viverra hendrerit diam in tempus. Etiam gravida justo
+                        nec erat vestibulum, et malesuada augue laoreet.</p>
+                    <p class="mb-4">Pellentesque aliquam dolor eget urna ultricies tincidunt. Nam volutpat libero sit
+                        amet leo cursus, ac viverra eros tristique. Morbi quis quam mi. Cras vel gravida eros. Proin
+                        scelerisque quam nec elementum viverra. Suspendisse viverra hendrerit diam in tempus.</p>
+                    <a href="/" class="btn btn-secondary rounded-pill px-5 py-3 text-white">More Details</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+
+
+    <!-- Services Start -->
+    <div class="container-fluid services py-5 mb-5">
+        <div class="container">
+            <div class="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
+                <h5 class="text-primary">Konsentrasi Ilmu</h5>
+                <h1>Bidang Pengembangan dan Keilmuan Teknologi Informasi</h1>
+            </div>
+            <div class="row g-5 services-inner">
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".3s">
+                    <div class="services-item bg-light">
+                        <div class="p-4 text-center services-content">
+                            <div class="services-content-icon">
+                                <i class="fa fa-code fa-7x mb-4 text-primary"></i>
+                                <h4 class="mb-3">Web Design</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Sed efficitur quis purus ut
+                                    interdum. Aliquam dolor eget urna ultricies tincidunt.</p>
+                                <a href="" class="btn btn-secondary text-white px-5 py-3 rounded-pill">Read
+                                    More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".5s">
+                    <div class="services-item bg-light">
+                        <div class="p-4 text-center services-content">
+                            <div class="services-content-icon">
+                                <i class="fa fa-file-code fa-7x mb-4 text-primary"></i>
+                                <h4 class="mb-3">Web Development</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Sed efficitur quis purus ut
+                                    interdum. Aliquam dolor eget urna ultricies tincidunt.</p>
+                                <a href="" class="btn btn-secondary text-white px-5 py-3 rounded-pill">Read
+                                    More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-4 wow fadeIn" data-wow-delay=".7s">
+                    <div class="services-item bg-light">
+                        <div class="p-4 text-center services-content">
+                            <div class="services-content-icon">
+                                <i class="fas fa-laptop fa-7x mb-4 text-primary"></i>
+                                <h4 class="mb-3">Programming</h4>
+                                <p class="mb-4">Lorem ipsum dolor sit amet elit. Sed efficitur quis purus ut
+                                    interdum. Aliquam dolor eget urna ultricies tincidunt.</p>
+                                <a href="" class="btn btn-secondary text-white px-5 py-3 rounded-pill">Read
+                                    More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Services End -->
+
+
+    <!-- Blog Start -->
+    <div class="container-fluid blog py-5 mb-5">
+        <div class="container">
+            <div class="text-center mx-auto pb-5 wow fadeIn" data-wow-delay=".3s" style="max-width: 600px;">
+                <h5 class="text-primary">Info</h5>
+                <h1>Informasi & Berita Terbaru</h1>
+            </div>
+            <div class="row g-5 justify-content-center">
+                <div class="col-lg-6 col-xl-4 wow fadeIn" data-wow-delay=".7s">
+                    <div class="blog-item position-relative bg-light rounded">
+                        <img src="{{ asset('front-end/img/blog-3.jpg') }}" class="img-fluid w-100 rounded-top"
+                            alt="">
+                        <span class="position-absolute px-4 py-3 bg-primary text-white rounded"
+                            style="top: -28px; right: 20px;">Mobile App</span>
+                        <div class="blog-btn d-flex justify-content-between position-relative px-3"
+                            style="margin-top: -75px;">
+                            <div class="blog-icon btn btn-secondary px-3 rounded-pill my-auto">
+                                <a href="" class="btn text-white ">Read More</a>
+                            </div>
+                            <div class="blog-btn-icon btn btn-secondary px-4 py-3 rounded-pill ">
+                                <div class="blog-icon-1">
+                                    <p class="text-white px-2">Share<i class="fa fa-arrow-right ms-3"></i></p>
+                                </div>
+                                <div class="blog-icon-2">
+                                    <a href="" class="btn me-1"><i class="fab fa-facebook-f text-white"></i></a>
+                                    <a href="" class="btn me-1"><i class="fab fa-twitter text-white"></i></a>
+                                    <a href="" class="btn me-1"><i class="fab fa-instagram text-white"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog-content text-center position-relative px-3" style="margin-top: -25px;">
+                            <img src="img/admin.jpg" class="img-fluid rounded-circle border border-4 border-white mb-3"
+                                alt="">
+                            <h5 class="">By Daniel Martin</h5>
+                            <span class="text-secondary">30 jan 2023</span>
+                            <p class="py-2">Lorem ipsum dolor sit amet elit. Sed efficitur quis purus ut interdum.
+                                Aliquam dolor eget urna ultricies tincidunt libero sit amet</p>
+                        </div>
+                        <div
+                            class="blog-coments d-flex justify-content-between px-4 py-2 border bg-primary rounded-bottom">
+                            <a href="" class="text-white"><small><i
+                                        class="fas fa-share me-2 text-secondary"></i>5324 Share</small></a>
+                            <a href="" class="text-white"><small><i
+                                        class="fa fa-comments me-2 text-secondary"></i>5 Comments</small></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Blog End -->
+@endsection
